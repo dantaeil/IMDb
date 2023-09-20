daadd086fdbfd10e6560691b6f614d44f6a69243
dantaeil <danjohnson0037@gmail.com>
2023-09-19 23:34:59 -0400

    Package Update

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,4 +1,4 @@
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import clsx from "clsx";
 import { useState } from "react";
 
