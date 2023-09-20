240798e24250c9d66a8debc99b05e12d69c19e34
dantaeil <danjohnson0037@gmail.com>
2023-09-19 23:33:52 -0400

    Change HeroIcons Content

--- src/components/layouts/player.js
+++ src/components/layouts/player.js
@@ -8,8 +8,26 @@ const PlayLayout = ({ children }) => {
   };
   return (
     <div className="container pl-10 my-3">
-      <button className="text-2xl font-bold" onClick={goBack}>
-        &lt;Back
+      <button
+        className="text-2xl font-bold flex flex-row items-center"
+        onClick={goBack}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 22 22"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6 mr-1"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 19.5L8.25 12l7.5-7.5"
+          />
+        </svg>
+
+        <div className="flex-initial">BACK</div>
       </button>
       <div className="container mx-auto py-5 px-40">{children}</div>
     </div>
